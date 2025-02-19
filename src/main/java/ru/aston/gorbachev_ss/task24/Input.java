6de430a65a9d61df7cf5bc9fc1db3e92cb96d0df
stas-gorbachev <stasgorbachev96@gmail.com>
2025-02-19 17:42:43 +0300

    6 commit

--- src/main/java/ru/aston/gorbachev_ss/task24/Input.java
+++ src/main/java/ru/aston/gorbachev_ss/task24/Input.java
@@ -1,10 +1,20 @@
 package ru.aston.gorbachev_ss.task24;
 
 public class Input {
+
+    int integer = 108;
 }
 
-class Output {}
+class Output extends Input {
+
+    void print() {
+        System.out.println("Целое число: " + integer);
+    }
+}
 
 class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Output number = new Output();
+        number.print();
+    }
 }
