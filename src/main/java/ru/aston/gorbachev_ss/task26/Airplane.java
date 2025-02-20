9b927244866629bf1c4828240155fc0d0f4f6114
stas-gorbachev <stasgorbachev96@gmail.com>
2025-02-20 17:20:42 +0300

    8 commit

--- src/main/java/ru/aston/gorbachev_ss/task26/Airplane.java
+++ src/main/java/ru/aston/gorbachev_ss/task26/Airplane.java
@@ -2,21 +2,23 @@ package ru.aston.gorbachev_ss.task26;
 
 public class Airplane {
 
-    class Wing {
+    static class Wing {
 
-        static float weight = 497.7f;
+        float weight = 497.7f;
 
-        static void print() {
+        void print() {
             System.out.println("Вес крыла: " + weight + " кг.");
         }
     }
 
-    Airplane airplane = new Airplane();
-}
-
-class Main {
     public static void main(String[] args) {
-        Airplane.Wing.print();
-//        Airplane.Wing wing1 = new Airplane();
+        Wing wing = new Wing();
+        wing.print();
+        Wing wing1 = new Wing();
+        Wing wing2 = new Wing();
+        wing1.weight = 654.1f;
+        wing2.weight = 249.5f;
+        wing1.print();
+        wing2.print();
     }
 }
