549df7557ee99f826ae0bb6452dba142d7ccabb4
stas-gorbachev <stasgorbachev96@gmail.com>
2025-05-08 00:32:06 +0300

    jdbc_task

--- src/main/java/ru/aston/gorbachev_ss/jdbc_task/Library.java
+++ src/main/java/ru/aston/gorbachev_ss/jdbc_task/Library.java
@@ -16,6 +16,8 @@ public class Library {
 //                addNewBook(statement);
 //                addNewReader(statement);
                 booksOutput(statement);
+                readersOutput(statement);
+                authorsOutput(statement);
                 statement.close();
             }
         } catch (SQLException exception) {
@@ -24,20 +26,39 @@ public class Library {
     }
 
     public static void addNewBook(Statement statement) throws SQLException {
-        statement.executeUpdate("INSERT INTO books (title, author_id, published_year) VALUES ('Война и мир', 1, 1869)");
+        statement.executeUpdate("INSERT INTO books (title, author_id, published_year) VALUES ('Портрет Дориана Грея', 2, 1891)");
     }
 
     public static void addNewReader(Statement statement) throws SQLException {
-        statement.executeUpdate("INSERT INTO readers (first_name, last_name, email) VALUES ('Влад', 'Иванов', 'vlad.ivanov@gmail.com')");
+        statement.executeUpdate("INSERT INTO readers (first_name, last_name, email) VALUES ('Виктор', 'Павлов', 'viktor.pavlov@mail.ru')");
     }
 
     public static void booksOutput(Statement statement) throws SQLException {
         ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
         while (resultSet.next()) {
             System.out.println("ID книги: " + resultSet.getInt("id") +
-                    ", Название: " + resultSet.getString("title") +
-                    ", ID автора: " + resultSet.getInt("author_id") +
-                    ", Год публикации: " + resultSet.getInt("published_year"));
+                    " | Название: " + resultSet.getString("title") +
+                    " | ID автора: " + resultSet.getInt("author_id") +
+                    " | Год публикации: " + resultSet.getInt("published_year"));
+        }
+    }
+
+    public static void readersOutput(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM readers");
+        while (resultSet.next()) {
+            System.out.println("ID читателя: " + resultSet.getInt("id") +
+                    " | Имя: " + resultSet.getString("first_name") +
+                    " | Фамилия: " + resultSet.getString("last_name") +
+                    " | Электронная почта: " + resultSet.getString("email"));
+        }
+    }
+
+    public static void authorsOutput(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM authors");
+        while (resultSet.next()) {
+            System.out.println("ID автора: " + resultSet.getInt("id") +
+                    " | Имя: " + resultSet.getString("first_name") +
+                    " | Фамилия: " + resultSet.getString("last_name"));
         }
     }
 }
