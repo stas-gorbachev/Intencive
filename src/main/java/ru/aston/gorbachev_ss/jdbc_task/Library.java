fe7ba3ffd360d9a9a64c8df69564e0eaf538c772
stas-gorbachev <stasgorbachev96@gmail.com>
2025-05-12 01:46:09 +0300

    jdbc_task

--- src/main/java/ru/aston/gorbachev_ss/jdbc_task/Library.java
+++ src/main/java/ru/aston/gorbachev_ss/jdbc_task/Library.java
@@ -1,6 +1,7 @@
 package ru.aston.gorbachev_ss.jdbc_task;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Library {
 
@@ -13,14 +14,15 @@ public class Library {
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             if (connection != null) {
                 Statement statement = connection.createStatement();
-//                addNewBook(statement);
-//                addNewReader(statement);
+                addNewBook(statement);
+                addNewReader(statement);
                 outputBooks(statement);
-//                outputReaders(statement);
-//                outputAuthors(statement);
-//                updateBook(connection);
+                outputReaders(statement);
+                outputAuthors(statement);
+                updateBook(connection);
                 deleteBook(connection);
-                outputBooks(statement);
+                addBookAndAuthor(connection, statement);
+                searchBook(connection);
                 statement.close();
             }
         } catch (SQLException exception) {
@@ -29,7 +31,7 @@ public class Library {
     }
 
     public static void addNewBook(Statement statement) throws SQLException {
-        statement.executeUpdate("INSERT INTO books (title, author_id, published_year) VALUES ('Портрет Дориана Грея', 2, 1891)");
+        statement.executeUpdate("INSERT INTO books (title, author_id, published_year) VALUES ('Вий', 10, 1835)");
     }
 
     public static void addNewReader(Statement statement) throws SQLException {
@@ -77,5 +79,44 @@ public class Library {
         preparedStatement.setInt(1, 2);
         preparedStatement.executeUpdate();
         preparedStatement.close();
+    }
+
+    public static void addBookAndAuthor(Connection connection, Statement statement) throws SQLException {
+        connection.setAutoCommit(false);
+        try {
+            statement.executeUpdate("INSERT INTO authors (first_name, last_name) VALUES ('Николай', 'Гоголь')",
+                    Statement.RETURN_GENERATED_KEYS);
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            int authorId = 0;
+            if (generatedKeys.next()) {
+                authorId = generatedKeys.getInt(1);
+                System.out.println("ID добавленного автора: " + authorId);
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO books (title, author_id, published_year) VALUES ('Мёртвые души', ?, 1842)");
+            preparedStatement.setInt(1, authorId);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            System.out.println("Ошибка при добавлении автора и книги: " + exception.getMessage());
+        }
+        connection.setAutoCommit(true);
+    }
+
+    public static void searchBook(Connection connection) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите фамилию автора: ");
+        String lastName = scanner.nextLine();
+        scanner.close();
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT title, published_year FROM authors, books WHERE author_id = authors.id AND last_name = ?");
+        preparedStatement.setString(1, lastName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        preparedStatement.close();
+        while (resultSet.next()) {
+            System.out.println("Название книги: " + resultSet.getString("title") +
+                    " | Год публикации: " + resultSet.getInt("published_year"));
+        }
     }
 }
