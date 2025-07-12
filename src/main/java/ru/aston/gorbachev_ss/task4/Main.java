b68c942a5a400fe85138612c0f04d704eb25d945
stas-gorbachev <stasgorbachev96@gmail.com>
2025-07-12 18:18:37 +0300

    task4

---
+++ src/main/java/ru/aston/gorbachev_ss/task4/Main.java
@@ -0,0 +1,19 @@
+package ru.aston.gorbachev_ss.task4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class Main {
+
+    ArrayList arrayList1 = new ArrayList();
+    ArrayList arrayList2 = new ArrayList(arrayList1);
+    ArrayList arrayList3 = new ArrayList(10);
+
+    HashMap hashMap1 = new HashMap();
+    HashMap hashMap2 = new HashMap(hashMap1);
+    HashMap hashMap3 = new HashMap(10);
+    HashMap hashMap4 = new HashMap(10, );
+
+    TreeSet treeSet1 = new TreeSet();
+}
